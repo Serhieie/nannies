a950146ae153e5a6f9ad80fcfbd68342755eaca8
Serhieie <89160714+Serhieie@users.noreply.github.com>
2024-06-17 16:30:09 +0000

    Deploying to gh-pages from @ Serhieie/nannies@391f1324241400eae6930ea32c6712445bda8136 🚀

--- NanniesPage-B58pA-r2.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./index-BB6ZpU0I.js";import{u as s}from"./useNannieState-pdFMLrZF.js";import{N as t}from"./Nannies-DLrppws3.js";const o=()=>{const{filteredNannies:e}=s();return n.jsx(n.Fragment,{children:n.jsx(t,{nannies:e})})};export{o as default};
