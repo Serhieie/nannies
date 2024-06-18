8f0919d6da915653f77dc3df366e442e51125d98
Serhiei <Serhieievb@gmail.com>
2024-06-18 07:41:43 +0300

    fix: skeletonStyles

--- src/components/Skelletons/NanniesListItemSkeleton/CardBottomPartSkeleton/CardBottomPartSkelleton.tsx
+++ src/components/Skelletons/NanniesListItemSkeleton/CardBottomPartSkeleton/CardBottomPartSkelleton.tsx
@@ -1,10 +1,10 @@
 export const CardBottomPartSkeleton: React.FC = () => {
   return (
     <div className="flex flex-col gap-[8px]">
-      <div className="h-5 w-full rounded-md bg-skin-grey opacity-10"></div>
-      <div className="h-5 w-[40%] rounded-md bg-skin-grey opacity-10"></div>
+      <div className="h-4 w-full rounded-md bg-skin-grey opacity-10"></div>
+      <div className="h-4 w-[40%] rounded-md bg-skin-grey opacity-10"></div>
 
-      <div className="mt-[8px] text-start text-skin-secondary underline">
+      <div className="mt-[12px] text-start text-skin-secondary underline">
         Read More
       </div>
     </div>
